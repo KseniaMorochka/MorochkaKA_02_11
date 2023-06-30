489193851aaa8a7d8bd7661bb796ccc0b0a91a83
ural-ECTS <ural@ects.ru>
2023-06-30 12:11:14 +0500

    first commit

---
+++ MorochkaKA_02_01/MorochkaKA_02_01/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MorochkaKA_02_01
+{
+    public partial class MainPage :ContentPage
+    {
+        public MainPage ()
+        {
+            InitializeComponent();
+            log.Text = Preferences.Get("login", "");
+            pass.Text = Preferences.Get("password", "");
+        }
+
+        private async void Button_Clicked (object sender, EventArgs e)
+        {
+            if(log.Text != "ects" || pass.Text != "ects123")
+            {
+                await DisplayAlert("Ошибка", "Заполните поля", "ok");
+            } 
+            else
+            {
+                Preferences.Set("login", $"{log.Text}");
+                Preferences.Set("password", $"{pass.Text}");
+            }
+            await Navigation.PushAsync(new SecondAcrivity());
+        }
+    }
+}
---
+++ MorochkaKA_02_01/MorochkaKA_02_01/SecondAcrivity.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MorochkaKA_02_01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondAcrivity :ContentPage
+    {
+        public SecondAcrivity ()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Clicked (object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FierdActivity());
+        }
+
+        private void pic_SelectedIndexChanged (object sender, EventArgs e)
+        {
+            if(pic.SelectedIndex == 0)
+            {
+                img.Source = "circle";
+            }
+            else if(pic.SelectedIndex == 1)
+            {
+                img.Source = "perim_treug";
+            } 
+            else
+            {
+                img.Source = "circle";
+            }
+        }
+    }
+}
